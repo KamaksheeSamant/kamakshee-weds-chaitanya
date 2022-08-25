20f543516afac8c5cea65affb0582236cb8b7dc9
KamaksheeSamant <111976475+KamaksheeSamant@users.noreply.github.com>
2022-08-25 12:43:54 +0000

    Deploying to gh-pages from @ KamaksheeSamant/kamakshee-weds-chaitanya@00854d745ecab946e8a46972ab7db1ff536beec0 🚀

--- _next/static/SoKlHxw9a9k5vOrBOr5kv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-46122092a910de63.js","static/css/7c9e31c2156353a4.css","static/chunks/pages/index-83af2f18f034876f.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
