8f35852c5b84d63a8b888116e722259403343c18
KamaksheeSamant <111976475+KamaksheeSamant@users.noreply.github.com>
2022-08-27 05:13:26 +0000

    Deploying to gh-pages from @ KamaksheeSamant/kamakshee-weds-chaitanya@e7ea3a7d4b5f2569822624c1a8619bc343ccb675 🚀

---
+++ _next/static/4-BuyfJLujM4PA4lEvhPd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-be56f150489b23d6.js","static/chunks/276-b1507e347ae69864.js","static/chunks/pages/index-8ddf62dcfe906f40.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/Q94q6al-TY31o_UYd0_bU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-328a73ec8687f02a.js","static/chunks/276-b1507e347ae69864.js","static/chunks/pages/index-e15c1900589c9477.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
