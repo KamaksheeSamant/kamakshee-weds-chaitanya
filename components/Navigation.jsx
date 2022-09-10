e103e6c9a0c2312737df995d6c90097c700c2894
Kamakshee Samant <kamaksheesamant@192-168-1-182.tpgi.com.au>
2022-09-10 21:45:28 +1000

    chore: add navigation content

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -9,14 +9,14 @@ function Navigation() {
   return (
     <div
       id="navigation"
-      className="w-full h-fit text-center pt-28 bg-orange-400"
+      className="w-full h-fit text-center pt-28 bg-orange-400 regardsBlockStart"
     >
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 justify-center items-center flex flex-col">
         <h2 className="py-6 text-[#fcf3c9]">Getting there</h2>
         <Vl color="#fcf3c9" />
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15087.679568863266!2d72.8340227!3d19.0232513!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c16493b9d0a73b7!2sSuryavanshi%20Kshatriya%20Sabhagruha%20And%20Banquet%20Hall!5e0!3m2!1sen!2sau!4v1662802535850!5m2!1sen!2sau"
-          width="600"
+          width="70%"
           height="450"
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
@@ -24,6 +24,7 @@ function Navigation() {
             marginRight: 0 + "em",
             borderLeft: "20px solid #d84242",
             borderTop: "20px solid #d84242",
+            maxWidth: "600px"
           }}
         ></iframe>
 
@@ -44,13 +45,13 @@ function Navigation() {
           </div>
         </div>
         <div className="flex align-center justify-around w-full pb-20">
-          <p className="py-4 text-lg tracking-widest text-[#fcf3c9] w-1/3">
+          <p className="p-4 text-lg tracking-widest text-[#fcf3c9] w-1/3">
             Drive 1.5 hours from San Jose
           </p>
-          <p className="py-4 text-lg tracking-widest text-[#fcf3c9] w-1/3">
+          <p className="p-4 text-lg tracking-widest text-[#fcf3c9] w-1/3">
             Drive 1.5 hours from San Jose
           </p>
-          <p className="py-4 text-lg tracking-widest text-[#fcf3c9] w-1/3">
+          <p className="p-4 text-lg tracking-widest text-[#fcf3c9] w-1/3">
             Drive 1.5 hours from San Jose
           </p>
         </div>
