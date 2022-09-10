1a751fb25170f270914b6160f5009e2f747cf46b
Kamakshee Samant <kamaksheesamant@192-168-1-182.tpgi.com.au>
2022-09-10 14:59:34 +1000

    chore: add wedding n our story pg

--- components/Story.jsx
+++ components/Story.jsx
@@ -10,7 +10,7 @@ function Story() {
         <h2 className="py-6 text-[#fcf3c9]">Our Story</h2>
         <p className="py-1 text-lg tracking-widest text-[#fcf3c9]">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industry standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
