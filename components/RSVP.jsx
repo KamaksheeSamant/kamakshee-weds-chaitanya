2748a3831d69f788bec33e7429b67f075cc1e141
Kamakshee Samant <kamaksheesamant@192-168-1-182.tpgi.com.au>
2022-09-15 21:42:07 +1000

    chore: added firebase n rsvp

--- components/RSVP.jsx
+++ components/RSVP.jsx
@@ -10,7 +10,7 @@ function RSVP() {
       className="w-full h-fit text-center pt-28 bg-[#f4c0af] regardsBlockStart"
     >
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 justify-center items-center">
-        <h2 className="py-6 text-[#cd9403]">Yes, I'll Be There</h2>
+        <h2 className="py-6 text-[#cd9403]">Yes, I will Be There</h2>
         <div className="md:w-1/2 lg:w-1/2 sm:w-full m-auto">
           <h4 className="py-1 text-lg tracking-widest text-[#daa520] text-start mt-4">
             <sup>*</sup>First name
